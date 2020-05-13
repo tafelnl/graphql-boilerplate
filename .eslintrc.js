fcb0c0e40854ae7ad9ef79f90140f2aff870cbf8
Tobyas <tobyas@tobyas.nl>
2020-05-13 16:19:23 +0200

    🔥 remove link-module-alias

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,16 +9,4 @@ module.exports = {
     ecmaVersion: 2020,
   },
   extends: ['eslint:recommended', 'plugin:node/recommended'],
-  settings: {
-    node: {
-      allowModules: ['~'],
-    },
-  },
-  // prefer the next rule exception above the global settings exception
-  // when this issue is solved: https://github.com/mysticatea/eslint-plugin-node/issues/217
-  // 'rules': {
-  //   'node/no-extraneous-require': ['error', {
-  //     'allowModules': ['~']
-  //   }],
-  // },
 };
